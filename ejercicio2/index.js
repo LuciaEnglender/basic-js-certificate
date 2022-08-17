a95b47863147dd6ba548196eff5123000cb4d32f
lenglender@auntap.com <lenglender@auntap.com>
2022-08-17 17:57:13 -0300

    second activity

---
+++ ejercicio2/index.js
@@ -0,0 +1,10 @@
+const name = 'Lucia';
+const age = 25;
+const isDeveloper = true;
+const birthdate = new Date(1997, 06, 07);
+const favoriteBook = {
+  titulo: 'The Lord if the Rings',
+  autor: 'Tolkien',
+  fecha: new Date(1954, 06, 29),
+  url: 'https://es.wikipedia.org/wiki/El_Se%C3%B1or_de_los_Anillos',
+};
