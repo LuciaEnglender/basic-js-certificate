edef1b3e1ecae4bc32aa6fb981a26e308a199989
lenglender@auntap.com <lenglender@auntap.com>
2022-08-17 18:48:02 -0300

    third activity

---
+++ ejercicio3/factorial-break.js
@@ -0,0 +1,9 @@
+let factorial = 1;
+let index = 1;
+while (factorial) {
+  if (index === 10) break;
+  factorial *= index;
+  index++;
+}
+
+console.log(factorial);
---
+++ ejercicio3/factorial-for.js
@@ -0,0 +1,5 @@
+var factorial = 1;
+for (let index = 1; index <= 10; index++) {
+  factorial *= index;
+}
+console.log(factorial);
