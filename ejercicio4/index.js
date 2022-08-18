3d72bf946a6b4135c77531442d7ae47b8b27302b
lenglender@auntap.com <lenglender@auntap.com>
2022-08-18 10:10:24 -0300

    fourth activity

---
+++ ejercicio4/index.js
@@ -0,0 +1,12 @@
+const myName = 'Lucía';
+const lastname = 'Englender';
+const estudiante = `${myName} ${lastname}`;
+const estudianteMayus = estudiante.toUpperCase();
+const estudianteMinus = estudiante.toLowerCase();
+const longitud = estudiante.length;
+const primeraLetraName = myName[0];
+const ultimaLetraApellido = lastname[lastname.length - 1];
+const sinEspacios = estudiante.trim();
+const existName = estudiante.includes(myName);
+
+
