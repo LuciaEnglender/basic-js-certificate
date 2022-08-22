103795a5727a224420d509603163947c202f177c
lenglender@auntap.com <lenglender@auntap.com>
2022-08-22 13:57:24 -0300

    fifth exercise done

--- ejercicio5/index.js
+++ ejercicio5/index.js
@@ -3,13 +3,4 @@ let alturaMetros = parseFloat(1.59);
 let pesoKg = parseFloat(55.2);
 let alturaRedondeada = Math.floor(alturaMetros);
 let pesoRedondeado = Math.ceil(pesoKg);
-let maxVal = Number.MAX_VALUE + 1;
-
-console.log(
-  alturaCentimetros,
-  alturaMetros,
-  alturaRedondeada,
-  pesoKg,
-  pesoRedondeado,
-  maxVal
-);
+let maxVal = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
